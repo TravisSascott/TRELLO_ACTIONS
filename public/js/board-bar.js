de3f4b7695637aa96ae3e1c3b6ed3e5c66345f36
Glitch (trello-power-up) <none>
2019-09-18 13:44:42 +0000

    👆⛰ Checkpoint ./public/js/board-bar.js:202855/676 ./public/board-bar.html:202855/134

--- public/js/board-bar.js
+++ public/js/board-bar.js
@@ -5,7 +5,15 @@ var t = TrelloPowerUp.iframe();
 // want to know when you are being closed?
 window.addEventListener('unload', function(e) {
   // Our board bar is being closed, clean up if we need to
+  console.log('In before unload event');
 });
+
+const button = document.getElementById('navigate');
+button.addEventListener('click', function(event){
+  t.navigate({
+    url: 'https://trello.com/c/nqPiDKmw/9-grand-canyon-national-park'
+  });
+})
 
 t.render(function(){
   // this function we be called once on initial load

afa490fdedb1acc678b67b78c3811ba703a6a705
Glitch (trello-power-up) <none>
2022-03-03 13:47:29 +0000

    🍤💃 Checkpoint ./public/js/client.js:28082340/59835

--- public/js/client_original.js
+++ public/js/client_original.js
@@ -322,21 +322,302 @@ var cardButtonCallback = function(t) {
 };
 
 // We need to call initialize to get all of our capability handles set up and registered with Trello
+TrelloPowerUp.initialize(
+  {
+    // NOTE about asynchronous responses
+    // If you need to make an asynchronous request or action before you can reply to Trello
+    // you can return a Promise (bluebird promises are included at TrelloPowerUp.Promise)
+    // The Promise should resolve to the object type that is expected to be returned
+    "board-buttons": function (t, opts) {
+      return t.board("id", "name").then(function (board) {
+        console.log(JSON.stringify(board, null, 2));
+        });
+      },
+    "attachment-sections": function(t, options) {
+      // options.entries is a list of the attachments for this card
+      // you can look through them and 'claim' any that you want to
+      // include in your section.
 
-  
-TrelloPowerUp.initialize({
-  "board-buttons": function (t, opts) {
-    return t.board("id", "name").then(function (board) {
-      console.log(JSON.stringify(board, null, 2));
-    });
+      // we will just claim urls for Yellowstone
+      var claimed = options.entries.filter(function(attachment) {
+        return attachment.url.indexOf("www.nps.gov/yell/") > -1;
+      });
+
+      // you can have more than one attachment section on a card
+      // you can group items together into one section, have a section
+      // per attachment, or anything in between.
+      if (claimed && claimed.length > 0) {
+        // if the title for your section requires a network call or other
+        // potentially length operation you can provide a function for the title
+        // that returns the section title. If you do so, provide a unique id for
+        // your section
+        return [
+          {
+            id: "Yellowstone", // optional if you aren't using a function for the title
+            claimed: claimed,
+            icon: GLITCH_ICON,
+            title: "Example Attachment Section: Yellowstone",
+            content: {
+              type: "iframe",
+              // You'll need to sign the URL to ensure that the iframe can continue to
+              // communicate with Trello.
+              url: t.signUrl("./section.html", {
+                arg: "you can pass your section args here"
+              }),
+              height: 230
+            }
+          }
+        ];
+      } else {
+        return [];
+      }
+    },
+    "attachment-thumbnail": function(t, options) {
+      // options.url has the url of the attachment for us
+      // return an object (or a Promise that resolves to it) with some or all of these properties:
+      // url, title, image, modified (Date), created (Date), createdBy, modifiedBy
+
+      // You should use this if you have useful information about an attached URL but it
+      // doesn't warrant pulling it out into a section via the attachment-sections capability
+      // for example if you just want to show a preview image and give it a better name
+      // then attachment-thumbnail is the best option
+      return {
+        url: options.url,
+        title: "👉 " + options.url + " 👈",
+        image: {
+          url: GLITCH_ICON,
+          logo: true // false if you are using a thumbnail of the content
+        }
+      };
+
+      // if we don't actually have any valuable information about the url
+      // we can let Trello know like so:
+      // throw t.NotHandled();
+    },
+    "board-buttons": function(t, options) {
+      return [
+        
+        {
+          // or we can also have a button that is just a simple url
+          // clicking it will open a new tab at the provided url
+          icon: WHITE_ICON,
+          text: "OLD_ACTIONS",
+          url: "https://braludo-powerup-test.glitch.me/actions.html",
+          target: "Inspiring Boards" // optional target for above url
+        },
+        {
+          // or we can also have a button that is just a simple url
+          // clicking it will open a new tab at the provided url
+          icon: WHITE_ICON,
+          text: "ACCIONES",
+          callback: function (t){
+            return t.popup({
+              title: "ACCIONES DEFINIDAS EN EL TABLERO",
+              url: "board_actions.html"
+            })
+          }
+        },
+        {
+          text: "Try Out t.navigate()",
+          condition: "always",
+          callback: function(t) {
+            return t.popup({
+              title: "t.navigate() Flavors",
+              items: [
+                {
+                  // https://trello.com/b/Yc8MHkrS/trello-power-up-test-board?menu=filter&filter=label:green
+                  text: "t.navigate(boardUrl) (internal)",
+                  callback: function(t, opts) {
+                    return t.navigate({
+                      url:
+                        "https://trello.com/b/Yc8MHkrS/trello-power-up-test-board?menu=filter&filter=label:green"
+                    });
+                  }
+                },
+                {
+                  // https://trello.com/b/Yc8MHkrS/trello-power-up-test-board?menu=filter&filter=label:green OR https://trello.com/b/K3k1CNLD/holiday-meal-planning?menu=filter&filter=label:Sides
+                  text: "t.navigate(boardUrl) (external)",
+                  callback: function(t, opts) {
+                    return t.navigate({
+                      url:
+                        "https://trello.com/b/K3k1CNLD/holiday-meal-planning?menu=filter&filter=label:Sides"
+                    });
+                  }
+                },
+                {
+                  // https://trello.com/c/3Zb4GlkC/2-card
+                  text: "t.navigate(cardUrl)",
+                  callback: function(t, opts) {
+                    return t.navigate({
+                      url: "https://trello.com/c/3Zb4GlkC/2-card"
+                    });
+                  }
+                }
+              ]
+            });
+          }
+        }
+      ];
+    },
+    "card-badges": function(t, options) {
+      return getBadges(t);
+    },
+    "card-back-section": function(t, options) {
+      return {
+        title: "My Card Back Section",
+        icon: GRAY_ICON, // Must be a gray icon, colored icons not allowed.
+        content: {
+          type: "iframe",
+          url: t.signUrl("./card-back-section.html"),
+          height: 230 // Max height is 500
+        }
+      };
+    },
+    "card-buttons": function(t, options) {
+      return [
+        {
+          // usually you will provide a callback function to be run on button click
+          // we recommend that you use a popup on click generally
+          icon: GRAY_ICON, // don't use a colored icon here
+          text: "Open Popup",
+          callback: cardButtonCallback
+        },
+        {
+          // but of course, you could also just kick off to a url if that's your thing
+          icon: GRAY_ICON,
+          text: "Just a URL",
+          url: "https://developers.trello.com",
+          target: "Trello Developer Site" // optional target for above url
+        },
+        {
+          icon: GRAY_ICON,
+          text: "Use REST API",
+          callback: restApiCardButtonCallback
+        },
+        {
+          // usually you will provide a callback function to be run on button click
+          // we recommend that you use a popup on click generally
+          icon: GRAY_ICON, // don't use a colored icon here
+          text: "Console LOG",
+          callback: function(t, options) {
+            console.log("It is loggiong...!");
+          }
+        },
+        {
+          icon: GRAY_ICON,
+          text: "CAJITA",
+          callback: function(t, options) {
+            return t.popup({
+              title: "POPITO",
+              url: "./popito.html",
+              hright: 250
+            });
+          }
+        }
+      ];
+    },
+    "card-detail-badges": function(t, options) {
+      return getBadges(t);
+    },
+    "card-from-url": function(t, options) {
+      // options.url has the url in question
+      // if we know cool things about that url we can give Trello a name and desc
+      // to use when creating a card. Trello will also automatically add that url
+      // as an attachment to the created card
+      // As always you can return a Promise that resolves to the card details
+
+      return new Promise(function(resolve) {
+        resolve({
+          name: "💻 " + options.url + " 🤔",
+          desc: "This Power-Up knows cool things about the attached url"
+        });
+      });
+
+      // if we don't actually have any valuable information about the url
+      // we can let Trello know like so:
+      // throw t.NotHandled();
+    },
+    "format-url": function(t, options) {
+      // options.url has the url that we are being asked to format
+      // in our response we can include an icon as well as the replacement text
+
+      return {
+        icon: GRAY_ICON, // don't use a colored icon here
+        text: "👉 " + options.url + " 👈"
+      };
+
+      // if we don't actually have any valuable information about the url
+      // we can let Trello know like so:
+      // throw t.NotHandled();
+    },
+    "show-settings": function(t, options) {
+      // when a user clicks the gear icon by your Power-Up in the Power-Ups menu
+      // what should Trello show. We highly recommend the popup in this case as
+      // it is the least disruptive, and fits in well with the rest of Trello's UX
+      return t.popup({
+        title: "Settings",
+        url: "./settings.html",
+        height: 184 // we can always resize later, but if we know the size in advance, its good to tell Trello
+      });
+    },
+
+    /*        
+      
+      🔑 Authorization Capabiltiies 🗝
+      
+      The following two capabilities should be used together to determine:
+      1. whether a user is appropriately authorized
+      2. what to do when a user isn't completely authorized
+      
+  */
+    "authorization-status": function(t, options) {
+      // Return a promise that resolves to an object with a boolean property 'authorized' of true or false
+      // The boolean value determines whether your Power-Up considers the user to be authorized or not.
+
+      // When the value is false, Trello will show the user an "Authorize Account" options when
+      // they click on the Power-Up's gear icon in the settings. The 'show-authorization' capability
+      // below determines what should happen when the user clicks "Authorize Account"
+
+      // For instance, if your Power-Up requires a token to be set for the member you could do the following:
+      return (
+        t
+          .get("member", "private", "token")
+          // Or if you needed to set/get a non-Trello secret token, like an oauth token, you could
+          // use t.storeSecret('key', 'value') and t.loadSecret('key')
+          .then(function(token) {
+            if (token) {
+              return { authorized: true };
+            }
+            return { authorized: false };
+          })
+      );
+      // You can also return the object synchronously if you know the answer synchronously.
+    },
+    "show-authorization": function(t, options) {
+      // Returns what to do when a user clicks the 'Authorize Account' link from the Power-Up gear icon
+      // which shows when 'authorization-status' returns { authorized: false }.
+
+      // If we want to ask the user to authorize our Power-Up to make full use of the Trello API
+      // you'll need to add your API from trello.com/app-key below:
+      let trelloAPIKey = "";
+      // This key will be used to generate a token that you can pass along with the API key to Trello's
+      // RESTful API. Using the key/token pair, you can make requests on behalf of the authorized user.
+
+      // In this case we'll open a popup to kick off the authorization flow.
+      if (trelloAPIKey) {
+        return t.popup({
+          title: "My Auth Popup",
+          args: { apiKey: trelloAPIKey }, // Pass in API key to the iframe
+          url: "./authorize.html", // Check out public/authorize.html to see how to ask a user to auth
+          height: 140
+        });
+      } else {
+        console.log(
+          "🙈 Looks like you need to add your API key to the project!"
+        );
+      }
+    }
   },
-},
-  
-  
-  
-  
-  
-  
   {
     appKey: "your_key_here",
     appName: "My Trello App"
